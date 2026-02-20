e2218ce853a808c061f5271b90cd2068f05b425a
corsair <corsair954@mail.ru>
2026-02-20 15:14:27 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897]

---
+++ db/scripts/001_ddl_create_auto_user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE auto_user (
+    id SERIAL PRIMARY KEY,
+    login VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+);
---
+++ db/scripts/002_ddl_create_auto_post.sql
@@ -0,0 +1,6 @@
+CREATE TABLE auto_post (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR NOT NULL,
+    created TIMESTAMP NOT NULL,
+    auto_user_id INT REFERENCES auto_user(id) NOT NULL
+);
