bade13a295986d6b6cfd8dedb7bb451bd3d14647
corsair <corsair954@mail.ru>
2026-03-11 15:48:10 +0300

    1. Фильтры для объявлений машин [#4745]

---
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package ru.job4j.cars.repository;
+
+import ru.job4j.cars.model.Post;
+
+import java.util.List;
+
+public interface PostRepository {
+    List<Post> findLastDayPosts();
+
+    List<Post> findByBrand(String name);
+}
---
+++ src/main/java/ru/job4j/cars/repository/implementations/HbmPostRepository.java
@@ -0,0 +1,25 @@
+package ru.job4j.cars.repository.implementations;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.cars.model.Post;
+import ru.job4j.cars.repository.PostRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+public class HbmPostRepository implements PostRepository {
+    private final CrudRepository crudRepository;
+
+    @Override
+    public List<Post> findByBrand(String name) {
+        return crudRepository.query("FROM Post p JOIN FETCH Car c WHERE c.name = :pname", Post.class, Map.of("pname", name));
+    }
+
+    @Override
+    public List<Post> findLastDayPosts() {
+        LocalDateTime twentyFourHoursAgo = LocalDateTime.now().minusHours(24);
+        return crudRepository.query("FROM Post p WHERE p.created <= :pdate", Post.class, Map.of("pdate", twentyFourHoursAgo));
+    }
+}
